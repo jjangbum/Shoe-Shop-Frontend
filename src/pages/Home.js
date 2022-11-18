4f7c120b22395244bf369d1b21d609f4fc8ed105
k-kbk <kkbk-@naver.com>
2022-11-18 13:54:51 +0900

    design: Header, Home, Login 디자인 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = memo(() => {
     <>
       <Header></Header>
       <div className='h-screen w-4/5 block md:flex md:flex-row md:justify-center'>
-        <div className='h-auto w-11/12 sm:w-1/2 md:w-2/5 flex flex-col justify-center ml-0 md:ml-16 mt-36 md:mt-0'>
+        <div className='h-auto w-11/12 sm:w-1/2 md:w-2/5 flex flex-col justify-center md:ml-20 mt-36 md:mt-0'>
           <p className='text-3xl lg:text-4xl font-bold text-slate-800 leading-snug md:leading-normal lg:leading-normal'>
             세상의
           </p>
@@ -24,11 +24,11 @@ const Home = memo(() => {
             구경하러 가기!
           </Link>
         </div>
-        <div className='h-auto w-full md:w-1/2 flex items-center justify-center mt-6 md:mt-0'>
+        <div className='h-auto w-full md:w-1/2 flex items-center justify-center mt-6 sm:mt-0'>
           <img
             src={Shopping}
             alt='shopping'
-            className='h-64 w-64 lg:h-88 lg:w-88 xl:h-auto xl:w-auto animate-bounce-slow'
+            className='h-64 w-64 sm:h-72 sm:w-72 md:h-80 md:w-80 lg:h-88 lg:w-88 xl:h-auto xl:w-auto animate-bounce-slow'
           />
         </div>
       </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -17,37 +17,37 @@ const Login = memo(() => {
           <div className='mb-2 sm:mb-4'>
             <label
               htmlFor='id'
-              className='block text-md sm:text-lg font-medium leading-6 text-slate-900'>
+              className='block text-base sm:text-lg font-medium leading-6 text-slate-900'>
               아이디
             </label>
             <input
               id='id'
-              className='mt-2 appearance-none text-slate-800 bg-white rounded-md block w-full px-3 h-10 shadow-sm text-md sm:text-lg focus:outline-none placeholder:text-slate-400 focus:ring-blue-500 focus:ring-2 ring-1 ring-slate-200'
+              className='mt-2 appearance-none text-slate-800 bg-white rounded-md block w-full px-3 h-10 shadow-sm text-base sm:text-lg focus:outline-none placeholder:text-slate-400 focus:ring-blue-500 focus:ring-2 ring-1 ring-slate-200'
               type='text'
             />
           </div>
           <div className='mb-12 sm:mb-16'>
             <label
               htmlFor='pw'
-              className='block text-md sm:text-lg font-medium leading-6 text-slate-800'>
+              className='block text-base sm:text-lg font-medium leading-6 text-slate-800'>
               비밀번호
             </label>
             <input
               id='pw'
-              className='mt-2 appearance-none text-slate-900 bg-white rounded-md block w-full px-3 h-10 shadow-sm text-md sm:text-lg focus:outline-none placeholder:text-slate-400 focus:ring-blue-500 focus:ring-2 ring-1 ring-slate-200'
+              className='mt-2 appearance-none text-slate-900 bg-white rounded-md block w-full px-3 h-10 shadow-sm text-base sm:text-lg focus:outline-none placeholder:text-slate-400 focus:ring-blue-500 focus:ring-2 ring-1 ring-slate-200'
               type='password'
             />
           </div>
-          <button className='w-full bg-slate-700 text-white text-md sm:text-lg font-semibold py-3 px-24 mb-8 rounded-lg'>
+          <button className='w-full bg-slate-700 text-white text-base sm:text-lg font-semibold py-3 px-20 mb-8 rounded-lg'>
             로그인
           </button>
           <div className='flex flex-row justify-center mb-2'>
-            <p className='text-slate-600 text-md sm:text-md font-medium'>
+            <p className='text-slate-600 text-sm sm:text-base font-medium'>
               아직 회원이 아니신가요?
             </p>
             <Link
               to='/join'
-              className='text-md sm:text-md text-slate-700 font-medium underline pl-3'>
+              className='text-sm sm:text-base text-slate-700 font-medium underline pl-3'>
               회원가입
             </Link>
           </div>
