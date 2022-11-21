9517d9abeec66f0dc309402e5c8c6526f4ebd754
k-kbk <kkbk-@naver.com>
2022-11-21 15:20:50 +0900

    feat: 모바일 상단 메뉴바 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React, { memo } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { Disclosure } from '@headlessui/react';
+import { Bars4Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import Logo from '../assets/xing.png';
-import Menu from '../assets/menu.png';
 
 const Header = memo(({ loggedIn, setLoggedIn }) => {
   // 로그아웃
@@ -19,40 +20,90 @@ const Header = memo(({ loggedIn, setLoggedIn }) => {
   };
 
   return (
-    <header className='flex items-center justify-between w-full px-6 lg:px-40 py-4 fixed z-10 border-b border-neutral-300 drop-shadow-sm bg-neutral-50'>
-      <div className='flex flex-row items-center'>
-        <Link to='/'>
-          <img src={Logo} alt='logo' className=' h-7 w-7 sm:h-8 sm:w-8' />
-        </Link>
-        <p className='px-4 text-neutral-400 hidden sm:block sm:text-xl'>|</p>
-        <div className='flex flex-row items-center'></div>
-      </div>
-      <div className='block sm:hidden'>
-        <img src={Menu} alt='logo' className=' h-6 w-6' />
-      </div>
-      <div className='hidden sm:block'>
-        {loggedIn ? (
-          <>
-            <Link
-              to='/cart'
-              className='text-neutral-900 text-base font-medium px-4 py-2 rounded-md hover:opacity-50'>
-              장바구니
+    <Disclosure>
+      {({ open }) => (
+        <header className='w-full flex flex-col sm:flex-row px-6 lg:px-40 py-4 fixed z-50 border-b rounded-b-lg border-neutral-300 drop-shadow-sm bg-neutral-50'>
+          <div className='w-full flex justify-between sm:items-center sm:justify-between'>
+            <Link to='/'>
+              <img src={Logo} alt='logo' className='h-8 w-8 sm:h-9 sm:w-9' />
             </Link>
-            <Link
-              className='text-neutral-900 text-base font-medium px-4 py-2 rounded-md hover:opacity-50'
-              onClick={handleLogout}>
-              로그아웃
-            </Link>
-          </>
-        ) : (
-          <Link
-            to='/login'
-            className='text-neutral-900 text-base font-medium px-4 py-2 rounded-md hover:opacity-50'>
-            로그인
-          </Link>
-        )}
-      </div>
-    </header>
+            <div className='hidden sm:block'>
+              {loggedIn ? (
+                <>
+                  <Link
+                    to='/cart'
+                    className='text-neutral-900 text-base font-medium px-4 py-2 rounded-md hover:opacity-50'>
+                    장바구니
+                  </Link>
+                  <Link
+                    className='text-neutral-900 text-base font-medium px-4 py-2 rounded-md hover:opacity-50'
+                    onClick={handleLogout}>
+                    로그아웃
+                  </Link>
+                </>
+              ) : (
+                <Link
+                  to='/login'
+                  className='text-neutral-900 text-base font-medium px-4 py-2 rounded-md hover:opacity-50'>
+                  로그인
+                </Link>
+              )}
+            </div>
+            <Disclosure.Button
+              className='flex sm:hidden  items-center'
+              as='div'>
+              {open ? (
+                <XMarkIcon className='block h-7 w-7' aria-hidden='true' />
+              ) : (
+                <Bars4Icon className='block h-7 w-7' aria-hidden='true' />
+              )}
+            </Disclosure.Button>
+          </div>
+          <Disclosure.Panel
+            className='sm:hidden flex flex-col mt-7 -mb-1 text-center'
+            as='div'>
+            {loggedIn ? (
+              <>
+                <Link
+                  to='/cart'
+                  className='w-full px-3 py-2 rounded-lg hover:bg-gray-200 hover:bg-opacity-70'>
+                  <Disclosure.Button
+                    as='div'
+                    className=' text-neutral-900 text-base font-medium'>
+                    장바구니
+                  </Disclosure.Button>
+                </Link>
+                <Link
+                  to='#'
+                  className='w-full px-3 py-2 rounded-lg hover:bg-gray-200 hover:bg-opacity-70'>
+                  <Disclosure.Button
+                    as='div'
+                    className=' text-neutral-900 text-base font-medium'>
+                    마이페이지
+                  </Disclosure.Button>
+                </Link>
+                <Disclosure.Button
+                  as='div'
+                  className='text-neutral-900 rounded-lg text-base font-medium px-3 py-2 hover:bg-gray-200 hover:bg-opacity-70'
+                  onClick={handleLogout}>
+                  로그아웃
+                </Disclosure.Button>
+              </>
+            ) : (
+              <Link
+                to='/login'
+                className='w-full px-3 py-2 rounded-lg hover:bg-gray-200 hover:bg-opacity-70'>
+                <Disclosure.Button
+                  as='div'
+                  className='text-neutral-900 text-base font-medium'>
+                  로그인
+                </Disclosure.Button>
+              </Link>
+            )}
+          </Disclosure.Panel>
+        </header>
+      )}
+    </Disclosure>
   );
 });
 
